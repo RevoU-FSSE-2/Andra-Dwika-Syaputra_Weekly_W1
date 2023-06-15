5f9080ed01115d600f46c34f5dc7e56d57d8c76b
Andra <andradwikasa@gmail.com>
2023-06-15 23:57:03 +0700

    edited index.html script.js Styles.css for better looking website

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let contactButton = document.getElementById("contactButton");
 let contactInfo = document.getElementById("contactInfo");
 
 contactButton.addEventListener("click", function() {
-  if (contactInfo.style.display === "none") {
-    contactInfo.style.display = "block"; 
-  } else {
-    contactInfo.style.display = "none"; 
-  }
+    if (contactInfo.style.display === "none") {
+        contactInfo.style.display = "block";
+        contactButton.textContent = "Hide contact info";
+    } else {
+        contactInfo.style.display = "none";
+        contactButton.textContent = "Contact me";
+    }
 });
